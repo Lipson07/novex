3c53160d9c13b1706563c9d2e0beeaf0713db7eb
botoli <standrey2007@gmail.com>
2026-01-18 23:04:39 +0300

    Fixed error in page Detail.Projects.tsx

--- frontend/src/Detail_Projects/Detail.Projects.tsx
+++ frontend/src/Detail_Projects/Detail.Projects.tsx
@@ -1,20 +1,17 @@
 import { useState } from 'react';
-import ProjectsData from '../UI/Projects/ProjectsPage';
+import { ProjectsData } from '../MockData/Projects.Mockdata';
 import styles from './Detail.Projects.module.scss';
 import { ArrowRightIcon } from '../UI/Icons';
 export default function DetailProject({ name }) {
-  const [projects, setProjects] = useState(ProjectsData);
+  const [Projects, setProjects] = useState(ProjectsData);
   return (
     <div className={styles.allDetailProject}>
-      {projects.filter(
-        (proj) =>
-          proj.title === name && (
-            <div className={styles.DetailProjectHeader}>
-              <ArrowRightIcon />
-              <span>Projects</span>/<span>{proj.title}</span>
-            </div>
-          ),
-      )}
+      {Projects.filter((proj) => proj.title === name).map((proj) => (
+        <div className={styles.DetailProjectHeader}>
+          <ArrowRightIcon />
+          <span>Projects</span>/<span>{proj.title}</span>
+        </div>
+      ))}
     </div>
   );
 }
