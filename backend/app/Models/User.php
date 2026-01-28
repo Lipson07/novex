aee43398fced88f13772923589ad0e250ef063d6
Lipson07 <opka0180@gmail.com>
2026-01-28 21:56:03 +0300

    full proj and new migration,update model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -18,20 +17,19 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-      'name',
-      'email',
-      'password',
-      'role',
-      'avatar',
-      'avatar_path',
-      'last_seen_at',
-      'is_online'
+        'name',
+        'email',
+        'password',
+        'role',
+        'avatar',
+        'avatar_path',
+        'last_seen_at',
+        'is_online'
 
     ];
-public function projects(){
-        return $this->belongsToMany(Project::class,'project_user')->withPivot('role')->withTimestamps()
-
-}
+    public function projects() {
+        return $this->belongsToMany(Project::class, 'project_user')->withPivot('role')->withTimestamps();
+    }
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -47,8 +45,7 @@ public function projects(){
      *
      * @return array<string, string>
      */
-    protected function casts(): array
-    {
+    protected function casts(): array {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
